79bded7ce7e5a1df38b5a3f1f5a3a88149fbc471
Jerome Terry <jerome.l.terry@gmail.com>
2015-12-13 18:35:03 -0330

    Adding Express Server

---
+++ index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var path = path = require('path');
+var ejs = require('ejs');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+if (app.get('env') === 'development') {
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('pages/error', {
+			message: err.message,
+			error: err
+		});
+	});
+}
+
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('pages/error', {
+		message: err.message,
+		error: {}
+	});
+});
+
+module.exports = app;
