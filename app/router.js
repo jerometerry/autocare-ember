0b3802aea50a30ab2ace61e292f09d3a11e07e14
Jerome Terry <jerome.l.terry@gmail.com>
2015-12-13 22:35:18 -0330

    Adding example routing

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('posts', function() {
+    this.route('post', { path: '/:post_id'});
+  });
 });
 
 export default Router;
